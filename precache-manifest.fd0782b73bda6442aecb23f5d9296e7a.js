002e44ed4ccf9ad1ab2a1b6de0f86a9c05634ea6
odgit <o.dmitricenko@gmail.com>
2020-03-17 23:05:18 +0100

    Updates

---
+++ precache-manifest.fd0782b73bda6442aecb23f5d9296e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d90e2472ed4e9224e9723980ca5041e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96d81697b46b025d71e7",
+    "url": "/static/css/main.ea4239d0.chunk.css"
+  },
+  {
+    "revision": "10dc1823b85dc007c2f4",
+    "url": "/static/js/2.7e9c325c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7e9c325c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d81697b46b025d71e7",
+    "url": "/static/js/main.62777bad.chunk.js"
+  },
+  {
+    "revision": "66eb49ec3f0e83dffae4",
+    "url": "/static/js/runtime-main.5b74b4d1.js"
+  }
+]);
